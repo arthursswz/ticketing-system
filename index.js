30b9e9b44a29e8e8307220941b197cd8525222d5
arthursswz <jaoenglish77@gmail.com>
2026-06-27 19:41:12 -0300

    Ticket creation feature completed

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const prompt = require('prompt-sync')();
 
 //criado o objeto chamado
-const chamados= [{
-    id:Number,
-    nome:String,
-    setor:String,
-    descricao:String,
-    status:String
-}]
+const chamados= [];
 
 //criando o menu com as opções e  variável opcao
 let opcao;
@@ -23,14 +17,30 @@ do{
 
      //parte lógica do sistema de chamados
    opcao =prompt("digite o que desejas fazer: " );
+   console.clear();
+
+
    switch(opcao){
      case "criar":
-        chamados.forEach((chamado) =>{
-          chamados.nome = prompt("Nome do colaborador(a): ");
-          chamados.setor = prompt("Qual o seu setor? ");
-          chamados.descricao = prompt("Descrição do problema: ");
-        })
-   }
+           const nome = prompt("Nome do colaborador(a): ");
+           const setor = prompt("Qual o seu setor? ");
+           const descricao =prompt("Descrição do problema: ");
+            
+
+            const chamadosNovos= {
+            id:chamados.length +1,
+            nome:nome,
+            setor:setor,
+            descricao: descricao,
+            status: "aberto"
+}
+//cadastrando os chamadados novos no meu array de chamados
+chamados.push(chamadosNovos);
+console.log("chamado cadastrado com sucesso");
+console.clear();
+console.log(chamados);
+
+}
 
 }while(opcao!="sair")
 
