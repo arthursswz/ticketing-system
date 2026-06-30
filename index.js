e2c7c8850db88ab5d837bfdacbaf728037b66b07
arthursswz <jaoenglish77@gmail.com>
2026-06-29 22:31:18 -0300

    Added functions to make the code more organized

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const prompt = require('prompt-sync')();
 
 //criado o objeto chamado
 const chamados= [];
-
 //criando o menu com as opções e  variável opcao
-let opcao;
 
-do{
-    //exibindo menu para o usuário
+function menu(){ 
+  //exibindo menu para o usuário
  console.log("----------------------")
  console.log("bem vindo usuário ao menu de opções chamado o que desejas fazer?");
  console.log("digite criar para cadastrar novos chamados");
@@ -18,14 +16,17 @@ do{
  console.log("----------------------")
 
      //parte lógica do sistema de chamados
-   opcao =prompt("digite o que desejas fazer: " );
+   const opcao1 = prompt("digite o que desejas fazer: " );
    console.clear();
+   return opcao1;
+ }
 
 
-   switch(opcao){
-    
-     case "criar":
-        let nome, setor, descricao;
+
+ function criarChamado(){
+
+    let nome, setor, descricao;
+
        do{ 
            nome = prompt("Nome do colaborador(a): ");
             
@@ -33,11 +34,16 @@ do{
           
            descricao = prompt("Descrição do problema: ");
            console.clear();
+           //exibindo mensagem caso algum camapo esteja vazio
+
+           if( nome.trim() === "" || descricao.trim() === "" || setor.trim() === ""){
+            console.log("Algum campo foi preenchido incorretamente. Preencha todos os dados novamente.");
+           }
              
            }while( nome.trim() === "" || descricao.trim() === "" || setor.trim() === "")
             
 
-            const chamadosNovos= {
+            const chamadoNovo= {
             id:chamados.length +1,
             nome:nome,
             setor:setor,
@@ -46,30 +52,40 @@ do{
 }
 //cadastrando os chamadados novos no meu array de chamados
     console.clear();
-    chamados.push(chamadosNovos);
+    chamados.push(chamadoNovo);
     console.log("chamado cadastrado com sucesso");
-    break;
+ }
 
-//lista todos os chamados no sistema
+ function listarChamado (){
 
-    case "listar":
-      chamados.forEach((chamado) => { 
-        if(chamados.length===0){
-            console.log("não há nenhum chamado cadastrado no sistema");
-            } else {
+
+    if(chamados.length === 0){
+    console.log("não há nenhum chamado cadastrado no sistema")
+    } else {
+        chamados.forEach((chamado ) => { 
                 console.log("----------------------");
                 console.log(`id do chamado ${chamado.id}`);
                 console.log(`nome do colaborador: ${chamado.nome}`);
                 console.log(`setor do colaborador(a): ${chamado.setor}`);
                 console.log(`descrição do problema: ${chamado.descricao}`);
                 console.log(`status do chamado: ${chamado.status}`);
-                console.log("----------------------")
-                 } }) 
-         break;
-//busca todos os chamados no array
-                case "buscar":
-            const buscar = prompt("digite o id do chamado para o sistema realizar a buscar: ");
-            let idb = Number(buscar);
+                console.log("----------------------") })
+}
+}
+
+
+function buscarChamado(){
+let buscar, idb
+          do{ 
+
+            buscar = prompt("digite o id do chamado para o sistema realizar a buscar: ");
+            idb = Number(buscar);
+
+            if(buscar.trim()==="" || isNaN(idb))
+                console.log("Voce preencheu o campo id de forma inválida, preencha novamente");
+
+        }while(buscar.trim()===""  || isNaN(idb));
+
             //procura no array o id especificado pelo o usuário
             const achou = chamados.find((chamado) => {
                  return chamado.id ===idb;
@@ -81,33 +97,79 @@ do{
             } else{
                 console.log("Chamado não encontrado");
             }
-              break;
 
-//procurando no array um chamado
+}
 
-              case "resolver":
 
-            const procurart = prompt("digite o id do chamado: ");
-            let procurar = Number(procurart);
+function resolverChamado(){
+let procurart, procurar;
 
-            const resolverChamado = chamados.find((chamado) =>{
+//pede o id do chamado
+            do { 
+            procurart = prompt("digite o id do chamado: ");
+            procurar = Number(procurart);
+
+                if(procurart.trim() === ""  || isNaN(procurar)) { 
+                    console.log("o campo id foi preenchido incorretamente. Preencha todos os dados novamente.")
+                    }
+                 } while(procurart.trim() === "" || isNaN(procurar)  );
+
+
+//procura os chamados no array
+            const buscarchama = chamados.find((chamado) =>{
                 if(chamado.id ===procurar){
                       return true
                 }
             })
 
-             if(resolverChamado){
-                resolverChamado.status = "resolvido";
+//altera o status do chamado
+
+             if(buscarchama){
+                buscarchama.status = "resolvido";
                 console.log("o chamado foi resolvido com sucesso")
-                console.log(resolverChamado);
+                console.log(buscarchama);
              } else{
                 console.log("o chamado não foi resolvido");
              }
-             break;
 
-    default:
-        console.log("opção invalida, digite outra");
-        break;
+}
+
+let opcao;
+
+do{
+ 
+opcao = menu();
+
+   switch(opcao){
+ 
+//cria um chamado no sistema   
+            case "criar":
+            criarChamado();
+            break;
+
+//lista todos os chamados no sistema
+
+            case "listar":
+            listarChamado();
+            break;
+
+//busca todos os chamados no array
+            case "buscar":
+            buscarChamado();
+            break;
+
+//procurando no array um chamado
+
+            case "resolver":
+            resolverChamado();
+            break;
+
+            case "sair":
+            break;
+
+            default:
+            console.log("opção invalida, digite outra");
+            break;
 
         
 
